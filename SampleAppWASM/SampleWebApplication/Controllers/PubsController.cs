74b9275561b0ce7367a8612e90a675e5e85007f9
masami <pea98258@gmail.com>
2025-01-12 21:45:51 +0900

    feat: sampleWASM

---
+++ SampleAppWASM/SampleWebApplication/Controllers/PubsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SampleWebApplication.Data;
+
+namespace SampleWebApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PubsController : ControllerBase
+    {
+        private IDbContextFactory<PubsDbContext> dbContextFactory { get; set; } = null!;
+
+        public PubsController(IDbContextFactory<PubsDbContext> dbContextFactory)
+        {
+            this.dbContextFactory = dbContextFactory;
+        }
+
+        [HttpGet("Authors")]
+        public async Task<List<Author>> GetAuthorsAsync()
+        {
+            using (var pubs = await dbContextFactory.CreateDbContextAsync())
+            {
+                return await pubs.Authors.ToListAsync();
+            }
+        }
+    }
+
+}
